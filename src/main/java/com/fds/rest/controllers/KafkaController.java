0a663bb1d0e3a0429876362f36b0396b1dd56553
yevgeniy.shaidur <insigh2pizzza@gmail.com>
2022-09-09 20:38:56 +0300

    update kafka producer

--- src/main/java/com/fds/rest/controllers/KafkaController.java
+++ src/main/java/com/fds/rest/controllers/KafkaController.java
@@ -1,9 +1,11 @@
 package com.fds.rest.controllers;
 
-import com.fds.rest.model.User;
 import com.fds.rest.services.impl.ProducerServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/v1/kafka")
